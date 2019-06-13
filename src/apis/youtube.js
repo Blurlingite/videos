d8c2b4cd6dcd83235413b05bff38882276082f10
Vishnu Dawah <vkdawah@gmail.com>
2019-06-13 18:03:26 -0400

    Got Key for Google API

---
+++ src/apis/youtube.js
@@ -0,0 +1,3 @@
+// 'KEY' is capital b/c we will not be changing this variable
+// On Google API's website we restricted access to this key to localhost:3000 so no one can steal the key to this API we are using
+const KEY = "AIzaSyDMAnsLpiwjr1sR5LSrGH3QvIfEv63Z-yA";
